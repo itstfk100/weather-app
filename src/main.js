55a0470eb5cfe844fb278337dad53927fd70bfd9
itsTFK100 <147454127+itstfk100@users.noreply.github.com>
2025-03-28 19:04:08 -0500

    '#'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import Particles from 'vue3-particles'
 
 const app = createApp(App)
 
+app.config.globalProperties.$axios = axios
 document.body.style.margin = '0';      // Elimina márgenes
 document.body.style.padding = '0';     // Elimina rellenos
 document.body.style.height = '100vh';  // Asegura que ocupe toda la altura de la ventana
